4bb3fde9644ed40c5716a1559be02ba8b9ab3590
ParkSungHee <di6223@naver.com>
2022-06-29 23:58:15 +0900

    [PGS] week1 완주하지 못한 선수

---
+++ sunghee/완주하지 못한 선수.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+string solution(vector<string> participant, vector<string> completion) {
+
+}
+
+int main() {
+
+}
