038e80b5df3ec18ce65de7e78bada97486f2a0ad
ParkSungHee <di6223@naver.com>
2022-07-04 21:52:39 +0900

    [PGS] week1 전화번호 목록

--- sunghee/전화번호 목록.cpp
+++ sunghee/전화번호 목록.cpp
@@ -1,14 +1,24 @@
 #include <iostream>
 #include <string>
-#include <algorithm>
 #include <vector>
-
+#include <algorithm>
 using namespace std;
 
-string solution(vector<string> participant, vector<string> completion) {
+bool solution(vector<string> phone_book) {
+    bool answer = true;
+    sort(phone_book.begin(), phone_book.end());
 
+    for (int i = 0; i < phone_book.size() - 1; i++) { //어떤 번호의 접두사를 확인하기 위함
+        if (phone_book[i + 1].find(phone_book[i]) == 0) {
+            answer = false;
+            break;
+        }
+    }
+
+    return answer;
 }
 
 int main() {
-
+    vector<string> phone_book = {"119", "97674223", "1195524421"};
+    cout << solution(phone_book);
 }
