dd5438ffbced09d2fefa6d83d0ee39f5230e074d
ParkSungHee <di6223@naver.com>
2022-07-04 20:35:58 +0900

    [PGS] week1 완주하지 못한 선수

--- sunghee/완주하지 못한 선수.cpp
+++ sunghee/완주하지 못한 선수.cpp
@@ -1,14 +1,31 @@
 #include <iostream>
 #include <string>
-#include <algorithm>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 string solution(vector<string> participant, vector<string> completion) {
+    sort(participant.begin(), participant.end());
+    sort(completion.begin(), completion.end());
+    string answer;
 
+    for (int i = 0; i < completion.size(); i++) {
+        if (participant[i] != completion[i]) {
+            answer = participant[i];
+            break;
+        }
+    }
+    if (answer.empty()) {
+        answer = participant[participant.size() - 1];
+    }
+
+    return answer;
 }
 
 int main() {
+    vector<string> participant = {"mislav", "stanko", "mislav", "ana"};
+    vector<string> completion = {"stanko", "ana", "mislav"};
 
+    cout << solution(participant, completion);
 }
